7310a3ee9907c49366bbb2ad583aad7bd8bd6eac
Martinez Garrido Angel Ivan <ben6085@gmail.com>
2021-07-08 14:46:12 -0500

    se importo el archivo de los componentes del header

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,5 +1,6 @@
 import Menu from '../../templates/menu.html';
 import '@styles/fuentes/fuentes.css';
+import '@styles/header-components/header-components.css';
 
 const loadMenu=()=>{
     const header=document.getElementById('header');
